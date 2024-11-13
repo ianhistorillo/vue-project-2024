8b435b7836a84e2022684c7d21b7928098497376
Christian Historillo <ianworks@Christians-MacBook-Pro.local>
2024-11-13 22:12:06 +0800

    Vue 3 App first commit

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+
+export default [
+  {
+    name: "app/files-to-lint",
+    files: ["**/*.{js,mjs,jsx,vue}"],
+  },
+
+  {
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
+  },
+
+  {
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: [],
+      },
+    ],
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+];
